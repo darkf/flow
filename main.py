ae8b381d728439f0245a1ba5083029533d5fd33a
darkf <lw9k123@gmail.com>
2013-05-25 06:38:57 -0700

    main: add physicsSystem, some components, and draw squares with pygame

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from flow import Entity, Component, system
 from engine import PygameGame
+import pygame
+
+# todo: be able to initialize component instances' values
 
 game = None # global because of laziness
 
+PositionComponent = Component(x=0, y=0)
+VelocityComponent = Component(vx=1, vy=1)
 SquareComponent = Component(size=30)
 
-@system([SquareComponent])
+@system([PositionComponent, VelocityComponent])
+def physicsSystem(entity):
+	entity.x += entity.vx
+	entity.y += entity.vy
+
+@system([PositionComponent, SquareComponent])
 def squareRenderSystem(entity):
 	print "square:", entity
-	# todo: render square
+	pygame.draw.rect(game.screen, (255, 0, 0), pygame.Rect(entity.x, entity.y, entity.size, entity.size))
 
 def main():
 	global game
 	game = PygameGame()
-	squareEntity = Entity(SquareComponent)
+	squareEntity = Entity(SquareComponent, PositionComponent, VelocityComponent)
 	game.scene('main', [
 		# entities
 		squareEntity
-	]).addSystem(squareRenderSystem)
+	]).addSystem(physicsSystem).addSystem(squareRenderSystem)
 	game.run()
 
 if __name__ == '__main__':
