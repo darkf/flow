37a9deae61caf3b02afba75d4d91a8e7a791aa90
darkf <lw9k123@gmail.com>
2013-05-25 22:07:16 -0700

    main: ColorComponent!

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ game = None # global because of laziness
 PositionComponent = Component(x=0, y=0)
 VelocityComponent = Component(vx=0, vy=0)
 SquareComponent = Component(size=30)
+ColorComponent = Component(r=0, g=0, b=0)
 
 @system([PositionComponent, VelocityComponent])
 def physicsSystem(entity):
@@ -15,13 +16,13 @@ def physicsSystem(entity):
 
 @system([PositionComponent, SquareComponent])
 def squareRenderSystem(entity):
-	#print "square:", entity
-	pygame.draw.rect(game.screen, (255, 0, 0), pygame.Rect(entity.x, entity.y, entity.size, entity.size))
+	color = (0, 0, 0) if not entity.has(ColorComponent) else (entity.r, entity.g, entity.b)
+	pygame.draw.rect(game.screen, color, pygame.Rect(entity.x, entity.y, entity.size, entity.size))
 
 def main():
 	global game
 	game = PygameGame()
-	squareEntity = Entity(SquareComponent, PositionComponent, VelocityComponent(vx=1, vy=1))
+	squareEntity = Entity(SquareComponent, PositionComponent, VelocityComponent(vx=1, vy=1), ColorComponent(r=255, g=0, b=0))
 	game.scene('main', [
 		# entities
 		squareEntity
