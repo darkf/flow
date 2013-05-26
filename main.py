a81c15edc10f64823fbf0cca79fe5ab832a8982b
darkf <lw9k123@gmail.com>
2013-05-25 22:34:53 -0700

    main: add circle

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ game = None # global because of laziness
 PositionComponent = Component(x=0, y=0)
 VelocityComponent = Component(vx=0, vy=0)
 SquareComponent = Component(size=30)
+CircleComponent = Component(radius=20)
 ColorComponent = Component(r=0, g=0, b=0)
 
-@system([PositionComponent, VelocityComponent])
+@system(PositionComponent, VelocityComponent)
 def physicsSystem(entity):
 	entity.x += entity.vx
 	entity.y += entity.vy
 
-@system([PositionComponent, SquareComponent])
+@system(PositionComponent, CircleComponent)
+def circleRenderSystem(entity):
+	pygame.draw.circle(game.screen, (255, 255, 0), (entity.x, entity.y), entity.radius)
+
+@system(PositionComponent, SquareComponent)
 def squareRenderSystem(entity):
 	color = (0, 0, 0) if not entity.has(ColorComponent) else (entity.r, entity.g, entity.b)
 	pygame.draw.rect(game.screen, color, pygame.Rect(entity.x, entity.y, entity.size, entity.size))
@@ -23,13 +28,18 @@ def main():
 	global game
 	game = PygameGame()
 	game.scene('main', [
+		# systems
+		physicsSystem, squareRenderSystem, circleRenderSystem,
+
 		# entities
 		# red square from top-left
 		Entity(SquareComponent, PositionComponent, VelocityComponent(vx=1, vy=1), ColorComponent(r=255, g=0, b=0)),
 		# green square from top-right
 		Entity(SquareComponent, PositionComponent(x=game.screenWidth - SquareComponent.size, y=0),
-			   VelocityComponent(vx=-1, vy=1), ColorComponent(r=0, g=255, b=0))
-	]).addSystem(physicsSystem).addSystem(squareRenderSystem)
+			   VelocityComponent(vx=-1, vy=1), ColorComponent(r=0, g=255, b=0)),
+		# circle
+		Entity(CircleComponent, PositionComponent(x=game.screenWidth / 2 - CircleComponent.radius), VelocityComponent(vy=1))
+	])
 	game.run()
 
 if __name__ == '__main__':
